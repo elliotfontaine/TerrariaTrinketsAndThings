3fa59178fc4ae0cce80250d24b4aa8e6416d5455
Elliot Fontaine <elliot.fontaine@gmail.com>
2022-11-13 20:45:10 +0100

    Initial mod repo customization

---
+++ src/main/java/net/thelunarian/ttandt/TerrariaTrinketsAndThings.java
@@ -0,0 +1,15 @@
+package net.thelunarian.ttandt;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TerrariaTrinketsAndThings implements ModInitializer {
+	public static final String MOD_ID = "ttandt";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/thelunarian/ttandt/TerrariaTrinketsAndThingsClient.java
@@ -0,0 +1,13 @@
+package net.thelunarian.ttandt;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TerrariaTrinketsAndThingsClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
